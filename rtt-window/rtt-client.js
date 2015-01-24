a1721ea2045aec0f60ab8188f94316cf8dd136c6
Zetsin <zetsin@gmail.com>
2015-01-24 23:00:14 +0800

    test window

---
+++ rtt-window/rtt-client.js
@@ -0,0 +1,131 @@
+var dgram = require('dgram')
+var config = require('./rtt.json')
+var f = require('util').format
+var packetSize = process.argv[2] ? parseInt(process.argv[2]) : 128
+var lostTimeout = process.argv[3] ? parseInt(process.argv[3]) : 250
+var windowLength = process.argv[4] ? parseInt(process.argv[4]) : 16
+var hSendPacketTimeout
+var lastPacket
+var byteSended = 0
+var byteSendedTotal = 0
+var packetReceivedCount = 0
+var startupDateTime = new Date().valueOf()
+var lastPacketSeq = 0
+
+console.log('rtt-client')
+console.log('packet size ' + packetSize + ' B, lost timeout ' + lostTimeout + ' ms')
+/*
+var socket = dgram.createSocket('udp4')
+socket.bind(function() {
+	console.log('server_host = ' + config.client.server_host)
+	console.log('server_port = ' + config.client.server_port)
+	socket.on('message', onMessage)
+	sendPacket()
+	calcSpeed()
+})
+*/
+sendPacket()
+calcSpeed()
+
+function calcSpeed() {
+	var t = 1
+	setInterval(function() {
+		var speed = (byteSended / t).toFixed(0)
+		var speedAverage = (byteSendedTotal * 1000 / (new Date().valueOf() - startupDateTime)).toFixed(0)
+		var lostRate = (packetReceivedCount / (lastPacketSeq + 1) * 100).toFixed(2)
+		//console.log('speed %s B/s, lost %s%%', speed, lostRate)
+		console.log('speed %s B/s, avrage speed %s B/s, sended %s packets, received %s packets, lost rate %s', speed, speedAverage, lastPacketSeq + 1, packetReceivedCount, lostRate)
+		byteSended = 0
+	}, t * 1000)
+}
+
+function sendPacket() {
+
+	var beginTime = new Date().valueOf()
+	for(var i = 0; i < windowLength; i++) {
+		var packet = requestPacket()
+		var socket = dgram.createSocket('udp4')
+		socket.on('message', function (msg, rinfo) {
+			var packet = parsePacket(msg)
+			var rtt = (new Date().valueOf()) - packet.clientStamp
+
+			if(rtt > lostTimeout) return
+
+			byteSended += msg.length
+			byteSendedTotal += msg.length
+			packetReceivedCount++
+
+		})
+		socket.send(packet, 0, packet.length, config.client.server_port, config.client.server_host, function(err) {
+			lastPacketSeq++
+			if (err) {
+				console.log(err.toString())
+				return
+			}
+			//console.log('packet ' + packet.seq + ' sended')
+		})
+	}
+	setTimeout(sendPacket, lostTimeout)
+
+/*
+	var packet = requestPacket()
+	lastPacket = packet
+
+	hSendPacketTimeout = setTimeout(onSendPacketTimeout, lostTimeout)
+	socket.send(packet, 0, packet.length, config.client.server_port, config.client.server_host, function(err) {
+		if (err) {
+			console.log(err.toString())
+			return
+		}
+		//console.log('packet ' + packet.seq + ' sended')
+	})
+*/
+}
+
+function onMessage(msg, rinfo) {
+	var packet = parsePacket(msg)
+
+	if (lastPacket && packet.seq !== lastPacket.seq) {
+		// ignore
+		return
+	}
+
+	clearTimeout(hSendPacketTimeout)
+	byteSended += msg.length
+	byteSendedTotal += msg.length
+	packetReceivedCount++
+
+	var rtt = (new Date().valueOf()) - packet.clientStamp
+	// console.log(f('[%s] rtt = %s (ms)', packet.seq, rtt))
+	//setTimeout(sendPacket, 0)
+	sendPacket()
+}
+
+function onSendPacketTimeout() {
+	// console.log(f('packet %s timeout', lastPacket.seq))
+	sendPacket()
+}
+
+function requestPacket() {
+	if (requestPacket.nextSeq === undefined) {
+		requestPacket.nextSeq = 0
+	}
+	var buf = new Buffer(packetSize)
+	var seq = requestPacket.nextSeq++
+	var clientStamp = new Date().valueOf()
+	buf.writeInt32LE(seq, 0)
+	buf.writeDoubleLE(clientStamp, 4)
+	buf.seq = seq
+	buf.clientStamp = clientStamp
+	return buf
+}
+
+function parsePacket(buf) {
+	var seq = buf.readInt32LE(0)
+	var clientStamp = buf.readDoubleLE(4)
+	var serverStamp = buf.readDoubleLE(12)
+	buf.seq = seq
+	buf.clientStamp = clientStamp
+	buf.serverStamp = serverStamp
+	return buf
+}
